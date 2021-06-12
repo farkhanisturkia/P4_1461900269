34ff5f4ef55bd12facba27eb7120ac83b335700b
farkhanisturkia <farkhan22032000@gmail.com>
2021-06-12 14:52:16 +0700

    alhamdulillah

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Excel;
+use App\Exports\UsersExport;
+
+class BukuController
+{
+    public function index()
+    {
+        $semua = DB::table('rak_buku')
+        ->Join('buku', 'rak_buku.id_buku', '=', 'buku.id')
+        ->Join('jenis_buku', 'rak_buku.id_jenis_buku', '=', 'jenis_buku.id')
+        ->select('rak_buku.id','buku.judul AS judul_buku','buku.tahun_terbit AS tahun_buku','jenis_buku.jenis AS jenis_b',)
+        ->orderBy('rak_buku.id', 'asc')
+        ->get();
+
+    	return view('tampilan0269',['rak_buku' => $semua]);
+    }
+
+    public function export() 
+    {
+        return Excel::download(new UsersExport, 'Buku.xlsx');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/tampilan0269.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tampilkan Data</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body> 
+    <div>
+        <header>
+		    <li><a href="/print0269">Print</a></li>
+        </header>
+        <div class="menu-tengah">
+            <div class="kotak">
+	            <h3>Data Buku</h3><br>
+                <center>
+                <table border="1">
+		            <tr>
+			            <th>Nomor Rak</th>
+			            <th>Judul Buku</th>
+			            <th>Tahun Buku</th>
+                        <th>Jenis Buku</th>
+		            </tr>
+		            @foreach($rak_buku as $rb)
+		            <tr>
+			            <td>{{ $rb->id }}</td>
+			            <td>{{ $rb->judul_buku }}</td>
+                        <td>{{ $rb->tahun_buku }}</td>
+                        <td>{{ $rb->jenis_b }}</td>
+		            </tr>
+		            @endforeach
+	            </table></center>
+	        </div>
+        </div>
+    </div>
+</body>
+</html>
